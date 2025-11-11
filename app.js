7f3d3caf0924630e17d760bcc85d5c92996fd373
BalkanJepbarow <balkanjepbarow@gmail.com>
2025-11-11 17:48:21 +0300

    5 commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var config = {
   streamingAssetsUrl: "StreamingAssets",
   companyName: "DefaultCompany",
   productName: "Bear Clicker",
-  productVersion: "2.0"
+  productVersion: "1.0"
 };
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
